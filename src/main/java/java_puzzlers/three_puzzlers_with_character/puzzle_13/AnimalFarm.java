cd271ba053a1a38833f5354051e3317998910a05
Anshul Chauhan <anshul101289@gmail.com>
2022-08-20 13:48:34 +0530

    puzzles

--- src/main/java/java_puzzlers/three_puzzlers_with_character/puzzle_13/AnimalFarm.java
+++ src/main/java/java_puzzlers/three_puzzlers_with_character/puzzle_13/AnimalFarm.java
@@ -2,10 +2,29 @@ package java_puzzlers.three_puzzlers_with_character.puzzle_13;
 
 public class AnimalFarm {
     public static void main(String[] args) {
+
+        String str1 = "anshul";
+        String str2 = "anshul";
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+
+        System.out.println("***************************");
+
         final String pig = "length: 10";
         final String dog = "length: " + pig.length();
-
-        System.out.println("Animals are equal: "
-                           + pig == dog);
+        System.out.println("Animals are equal: "+(pig == dog));
+        System.out.println("Animals are equal: "+(pig.equals(dog)));
     }
 }
+
+// String Interning is a method of storing only one copy of each distinct String Value,
+// which must be immutable.
+
+// == tests whether they refer to precisely the same object
+
+// When comparing object references, u shd use the equals method in preference
+// to the == operator unless u need to compare object identity rather than value
+
+// Your code shd rarely, if ever, depend on the interning of string constants.Interning
+// was designed solely to reduce the memory footprint of VM, not as  a tool for devs
+
--- src/main/java/java_puzzlers/three_puzzlers_with_character/puzzle_15/Test.java
+++ src/main/java/java_puzzlers/three_puzzlers_with_character/puzzle_15/Test.java
@@ -1,8 +1,14 @@
 package java_puzzlers.three_puzzlers_with_character.puzzle_15;
 
 public class Test {
+    /*
+    *anshul \uffff
+    * this program will give "illegal unicode escape error"
+    * */
     public static void main(String[] args) {
         System.out.print("Hell");
         System.out.println("o world");
     }
 }
+
+// unicode escape must be well formed even if they appear in comments
